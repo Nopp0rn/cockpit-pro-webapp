e9705de9215e0d9ae132ea51024202ec19878680
Noppone Sripongpran <169129646+Nopp0rn@users.noreply.github.com>
2026-05-20 17:27:39 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',   // explicit — copies public/ → dist/
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  }
 })
